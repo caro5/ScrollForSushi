e351bfff4e78f8adb20688ed953e630e21395f12
Caroline Wong <carowong5@gmail.com>
2014-11-23 01:48:06 -0800

    testing

---
+++ ScrollForSushi/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewController.swift
+//  ScrollForSushi
+//
+//  Created by Caroline Wong on 11/22/14.
+//  Copyright (c) 2014 madebycaro. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet var welcomeLabel: UILabel!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
